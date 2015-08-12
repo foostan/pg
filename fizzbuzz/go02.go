60c177d3b9646eafd8fc72fef30466f0f44da802
foostan <ks@fstn.jp>
2015-08-13 01:12:39 +0900

    add fizzbuzz in go v02

---
+++ fizzbuzz/go02.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	for i := 1; i <= 100; i++ {
+
+		res := func(i int) string {
+			switch {
+			case i%15 == 0:
+				return "FizzBuzz"
+			case i%3 == 0:
+				return "Fizz"
+			case i%5 == 0:
+				return "Buzz"
+			default:
+				return strconv.Itoa(i)
+			}
+		}(i)
+		fmt.Println(res)
+	}
+}
